9c5a893bd7730ee2d4773f1a8fddd53510133a59
Elfin Git <musongoletracy@gmail.com>
2022-10-04 15:25:16 +0200

    fetch and render missions

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,5 +1,73 @@
-export default function Missions() {
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import './Missions.css';
+import { joinMission, fetchMissions, leaveMission } from '../../redux/mission/missions';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missionsReducer);
+
+  useEffect(() => {
+    let currentMissions = true;
+    if (currentMissions) {
+      dispatch(fetchMissions());
+      currentMissions = !currentMissions;
+    }
+  }, [dispatch]);
+
+  const handleJoin = (id) => {
+    dispatch(joinMission(id));
+  };
+
+  const handleLeave = (id) => {
+    dispatch(leaveMission(id));
+  };
+
   return (
-    <h1>Missions</h1>
+    <section id="missions">
+      <table id="mission-table">
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{'  '}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.name}</td>
+              <td>{mission.description}</td>
+              {mission.reserved ? (
+                <>
+                  <td>
+                    <Button variant="outline-secondary" className="btn btn-secondary" onClick={handleJoin(mission.id)}>Not a member</Button>
+                  </td>
+                  <td>
+                    <Button variant="outline-primary" className="btn btn-primary" onClick={handleLeave(mission.id)}>Join Mission</Button>
+                  </td>
+                </>
+              ) : (
+                <>
+                  <td>
+                    <Button variant="outline-secondary" className="btn btn-secondary">Active member</Button>
+                  </td>
+                  <td>
+                    <Button variant="outline-primary" className="btn btn-primary">Leave Mission</Button>
+                  </td>
+                </>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
-}
+};
+
+export default Missions;
---
+++ src/redux/mission/missions.js
@@ -0,0 +1,64 @@
+const baseURL = 'https://api.spacexdata.com/v3/missions';
+const FETCH_MISSIONS = 'missions/FETCH_MISSIONS';
+const JOIN_MISSION = 'missions/JOIN_MISSION';
+const LEAVE_MISSION = 'missions/LEAVE_MISSION';
+const initialState = [];
+
+export const joinMission = (id) => async (dispatch) => {
+  dispatch({
+    type: JOIN_MISSION,
+    payload: id,
+  });
+};
+
+export const leaveMission = (id) => async (dispatch) => {
+  dispatch({
+    type: LEAVE_MISSION,
+    payload: id,
+  });
+};
+
+export const fetchMissions = () => async (dispatch) => {
+  const response = await fetch(baseURL);
+  const { data } = await response.json();
+  dispatch({
+    type: FETCH_MISSIONS,
+    payload: data.map((mission) => ({
+      id: mission.mission_id,
+      name: mission.mission_name,
+      description: mission.description,
+      reserved: false,
+    })),
+  });
+};
+
+const missionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_MISSIONS:
+      return { ...state, mission: action.payload };
+    case JOIN_MISSION:
+      return {
+        ...state,
+        mission: state.mission.map((mission) => {
+          if (mission.id === action.payload) {
+            return { ...mission, reserved: true };
+          }
+          return mission;
+        }),
+      };
+    case LEAVE_MISSION:
+      return {
+        ...state,
+        mission: state.mission.map((mission) => {
+          if (mission.id === action.payload) {
+            return { ...mission, reserved: false };
+          }
+          return mission;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default missionsReducer;
