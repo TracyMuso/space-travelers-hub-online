be86147062f5135f75fc3a4d96593848d7b81675
tinayetsina <tinayetsina@outlook.com>
2022-10-07 02:20:44 +0800

    tests : added snapshot test

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Provider } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import MyProfile from './components/MyProfile/MyProfile';
 import Missions from './components/Missions/Missions';
-import Rockets from './components/Rockets/Rockets';
+import RocketsList from './components/Rockets/Rockets';
 import store from './redux/configureStore';
 
 const App = () => (
   <Provider store={store}>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Rockets />} />
+      <Route path="/" element={<RocketsList />} />
       <Route path="Missions" element={<Missions />} />
       <Route path="MyProfile" element={<MyProfile />} />
     </Routes>
